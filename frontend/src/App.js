e5f3dd57458a7b946646a6a7b0f8f9afadea4d39
Chloe-Baylock <chloebox523@gmail.com>
2021-12-05 19:24:37 -0500

    done with signup form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import LoginFormPage from "./components/LoginFormPage";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
 
 function App() {
@@ -13,8 +14,11 @@ function App() {
 
   return isLoaded && (
     <Switch>
-      <Route path="/login">
+      <Route path='/login'>
         <LoginFormPage />
+      </Route>
+      <Route path='/signup'>
+        <SignupFormPage />
       </Route>
     </Switch>
   );
